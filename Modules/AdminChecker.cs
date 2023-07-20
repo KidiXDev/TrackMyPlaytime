8da3307caf2ebda855ad4bc522e8eec7b8e5d822
Dikhi Ardiansyah <dikhiardian55@gmail.com>
2023-07-21 00:58:10 +0700

    Added admin checker function

---
+++ Modules/AdminChecker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Security.Principal;
+using System.Windows;
+
+namespace TMP.NET.Modules
+{
+    internal class AdminChecker
+    {
+        public bool IsUserAdministrator()
+        {
+            bool isAdmin;
+            try
+            {
+                WindowsIdentity user = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (Exception)
+            {
+                isAdmin = false;
+            }
+            return isAdmin;
+        }
+
+        public bool CheckWritePermission(string folderPath)
+        {
+            try
+            {
+                string testDirectory = Path.Combine(folderPath, Guid.NewGuid().ToString());
+                Directory.CreateDirectory(testDirectory);
+                Directory.Delete(testDirectory);
+                return true;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error checking folder permission: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
